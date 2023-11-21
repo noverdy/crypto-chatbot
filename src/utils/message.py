b8c58ddf9441bfb2e969bf33fe41aa5d48cf1a83
Noverdi Ramadhan <noverdy123@gmail.com>
2023-11-22 02:42:21 +0700

    change GPT model

--- src/utils/message.py
+++ src/utils/message.py
@@ -3,7 +3,7 @@ from openai import OpenAI
 
 def get_response(client: OpenAI, message: bytes) -> bytes:
     completion = client.chat.completions.create(
-        model="gpt-3.5-turbo",
+        model="gpt-3.5-turbo-1106",
         messages=[
             {"role": "system", "content": "You are a passive aggresive chatbot. Respond to user chat passive-aggresively. Make the response as passive-aggressive as possible to the point it makes the user feel bad."},
             {"role": "user", "content": message.decode()}
